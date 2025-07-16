8cfaf7297a95a90abc5f9a5176af21bd4b1d3a3b
haresh-sai06 <shareshsainaath@gmail.com>
2025-07-16 15:19:45 +0530

    feat: updated dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,9 @@ const App = () => (
           <div 
             className="fixed inset-0 z-0 bg-cover bg-center bg-no-repeat"
             style={{
-              backgroundImage: `url('https://images.unsplash.com/photo-1562774053-701939374585?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2072&q=80')`
+              backgroundImage: `url('https://image-static.collegedunia.com/public/reviewPhotos/389741/sri-krishna-college-of-technology-coimbatore-215488.jpg')`,
+              backgroundPosition: "center 70%"
+
             }}
           />
           
